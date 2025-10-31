2da0a949048b725a43874441baa210a3537b8aa5
arcathrax <ivanjoelpollak@proton.me>
2025-10-31 14:36:00 +0100

    feat(main): implement backend api to save config values

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 @app.route("/bluetooth/", methods=["GET", "POST"])
 def hello():
     if request.method == "POST":
-        name = request.args.get("capability", "KeyboardOnly")
-        return(f"capability: {escape(name)}")
+        config_file_manager = ConfigFileManager()
+
+        capability = request.args.get("capability", "KeyboardOnly")
+        config_file_manager.set_config_value("Bluetooth", "capability", capability)
+
+
+        return("Config saved.")
     else:
         config_file_manager = ConfigFileManager()
+
         return {
             "capability": config_file_manager.capability
         }
